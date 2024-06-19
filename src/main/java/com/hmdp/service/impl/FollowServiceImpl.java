97d46c05712c93a1e666d778530d0310c872e69b
haerker <haerker@foxmail.com>
2024-06-19 23:09:00 +0800

    关注功能

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,21 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +23,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result isFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("follow_user_id", followUserId).eq("user_id", userId).count();
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        if (isFollow) {
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(userId);
+            boolean isSuccess = save(follow);
+            if (isSuccess) stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+        } else {
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_user_id", followUserId));
+            if (isSuccess) stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+        }
+        return Result.ok();
+    }
 }
